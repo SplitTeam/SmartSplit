5bdfd8ce8fa483129e58125f106b8d5906236baf
inter <costache.i.mihailcristian24@stud.rau.ro>
2025-11-22 00:45:45 +0200

    Refactor import statements in main.tsx for cleaner syntax

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 
   import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
+  import App from "./App";
+  // @ts-ignore: allow importing CSS without type declarations
   import "./index.css";
 import React from "react";
-import * as React from "react";
 
   createRoot(document.getElementById("root")!).render(<App />);
